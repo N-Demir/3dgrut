6c8977c4c657066ed95ccdebe15a065f68874ea9
Nikita Demir <nikitde1@gmail.com>
2025-06-04 15:42:20 -0700

    wip

---
+++ run_server.py
@@ -0,0 +1,117 @@
+from pathlib import Path
+import socket
+import subprocess
+import threading
+import time
+import modal
+
+# app = modal.App("3dgrut", image=modal.Image.from_dockerfile(Path(__file__).parent / "Dockerfile"))
+app = modal.App("3dgrut", image=modal.Image.from_registry("nikitademir/3dgrut:latest")
+    # GCloud
+    #TODO: Install gcloud
+    .run_commands("apt-get update && apt-get install -y curl gnupg && \
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg && \
+    echo \"deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main\" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && \
+    apt-get update && apt-get install -y google-cloud-cli")
+    .add_local_file(Path.home() / "gcs-tour-project-service-account-key.json", "/root/gcs-tour-project-service-account-key.json", copy=True)
+    .run_commands(
+        "gcloud auth activate-service-account --key-file=/root/gcs-tour-project-service-account-key.json",
+        "gcloud config set project tour-project-442218",
+        "gcloud storage ls"
+    )
+    .env({"GOOGLE_APPLICATION_CREDENTIALS": "/root/gcs-tour-project-service-account-key.json"})
+    .run_commands("gcloud storage ls")
+    # # SSH server
+    .apt_install("openssh-server")
+    .run_commands(
+        "mkdir -p /run/sshd" #, "echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config", "echo 'root: ' | chpasswd" #TODO: uncomment this if the key approach doesn't work
+    )
+    .add_local_file(Path.home() / ".ssh/id_rsa.pub", "/root/.ssh/authorized_keys", copy=True)
+)
+
+
+LOCAL_PORT = 9090
+
+def wait_for_port(host, port, q):
+    start_time = time.monotonic()
+    while True:
+        try:
+            with socket.create_connection(("localhost", 22), timeout=30.0):
+                break
+        except OSError as exc:
+            time.sleep(0.01)
+            if time.monotonic() - start_time >= 30.0:
+                raise TimeoutError("Waited too long for port 22 to accept connections") from exc
+        q.put((host, port))
+
+
+@app.function(
+    timeout=3600 * 24,
+    gpu="T4",
+    secrets=[modal.Secret.from_name("wandb-secret"), modal.Secret.from_name("github-token")],
+    volumes={"/root/.cursor-server": modal.Volume.from_name("cursor-server", create_if_missing=True), 
+             "/root/data": modal.Volume.from_name("data", create_if_missing=True),
+             "/root/output": modal.Volume.from_name("output", create_if_missing=True)
+             }
+)
+def launch_ssh_server(q: modal.Queue):
+    with modal.forward(22, unencrypted=True) as tunnel:
+        host, port = tunnel.tcp_socket
+        threading.Thread(target=wait_for_port, args=(host, port, q)).start()
+
+        # Added these commands to get the env variables that docker loads in through ENV to show up in my ssh
+        import os
+        import shlex
+        from pathlib import Path
+
+        output_file = Path.home() / "env_variables.sh"
+
+        with open(output_file, "w") as f:
+            for key, value in os.environ.items():
+                escaped_value = shlex.quote(value)
+                f.write(f'export {key}={escaped_value}\n')
+        subprocess.run("echo 'source ~/env_variables.sh' >> ~/.bashrc", shell=True)
+
+        subprocess.run(["/usr/sbin/sshd", "-D"])  # TODO: I don't know why I need to start this here
+
+
+@app.function()
+def run_hello_world():
+    print("Hello, world!")
+    
+
+@app.local_entrypoint()
+def main(hello_world: bool = False):
+    if hello_world:
+        run_hello_world.remote()
+    else:
+        import sshtunnel
+
+        with modal.Queue.ephemeral() as q:
+            launch_ssh_server.spawn(q)
+            host, port = q.get()
+            print(f"SSH server running at {host}:{port}")
+
+            server = sshtunnel.SSHTunnelForwarder(
+                (host, port),
+                ssh_username="root",
+                ssh_password=" ",
+                remote_bind_address=("127.0.0.1", 22),
+                local_bind_address=("127.0.0.1", LOCAL_PORT),
+                allow_agent=False,
+            )
+
+            try:
+                server.start()
+                print(f"SSH tunnel forwarded to localhost:{server.local_bind_port}")
+
+                # TODO: This is a great idea, but for some reason it's buggy
+                # print("Opening up a cursor window...")
+                # subprocess.run("cursor --remote ssh://modal-server /workspace/", shell=True)
+
+                while True:
+                    time.sleep(1)
+            except KeyboardInterrupt:
+                print("\nShutting down SSH tunnel...")
+            finally:
+                server.stop()
